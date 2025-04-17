8e92121ca430ff969ab3a126b4c74108a9385c45
mat-3939 <znuakierkto@ymail.ne.jp>
2025-04-17 13:57:55 +0900

    4/17 松岡

---
+++ src/main/resources/schema/01_products.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS products (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    description VARCHAR(200),
+    price DECIMAL(10, 2) NOT NULL,
+    stock INTEGER NOT NULL,
+    imge_path TEXT NOT NULL,
+    category_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+); 
---
+++ src/test/java/com/example/mgmgapp/MgmgappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mgmgapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgmgappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
