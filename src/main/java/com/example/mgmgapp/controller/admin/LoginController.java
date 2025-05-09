c80d66c50f673e8b65535dce40164466708de694
mat-3939 <znuakierkto@ymail.ne.jp>
2025-05-09 13:40:05 +0900

    5/9 松岡 cssのファイル移動とフォルダ作成

--- src/main/java/com/example/mgmgapp/controller/admin/LoginController.java
+++ src/main/java/com/example/mgmgapp/controller/admin/LoginController.java
@@ -1,10 +1,10 @@
 package com.example.mgmgapp.controller.admin;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+// import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.mgmgapp.form.LoginForm;
